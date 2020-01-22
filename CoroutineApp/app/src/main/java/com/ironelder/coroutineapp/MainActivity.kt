f50082ff6f005ab991dc3570bd575f3292d21a80
Jason <ironelder@gmail.com>
2020-01-22 09:07:19 +0900

    ironelder-Coroutine 프로젝트 추가
    ironelder-Coroutine 프로젝트 추가

---
+++ CoroutineApp/app/src/main/java/com/ironelder/coroutineapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ironelder.coroutineapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
