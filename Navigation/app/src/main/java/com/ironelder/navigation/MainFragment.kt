f46459620a3b0716e182de18c775b5449fc96bc9
Jason <ironelder@gmail.com>
2020-02-13 12:11:40 +0900

    Navigation-예제 추가 commit
    Navigation-예제 추가 commit

--- Navigation/app/src/main/java/com/ironelder/navigation/MainFragment.kt
+++ Navigation/app/src/main/java/com/ironelder/navigation/MainFragment.kt
@@ -6,14 +6,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(),View.OnClickListener {
+
+    lateinit var navController:NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        mainBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        navController.navigate(R.id.firstPageFragment)
     }
 
 
