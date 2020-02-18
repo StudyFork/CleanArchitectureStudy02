0d38a0e0410b32b88c036a98b25ed2adfaf80139
taeiim <taeiim.god@gmail.com>
2020-02-18 19:01:00 +0900

    MoviePopular Model 추가

---
+++ MovieApp/app/src/main/java/com/god/taeiim/movieapp/api/model/MoviePopular.kt
@@ -0,0 +1,25 @@
+package com.god.taeiim.movieapp.api.model
+
+data class MoviePopular(
+    val page: Int,
+    val results: List<Result>,
+    val total_pages: Int,
+    val total_results: Int
+)
+
+data class Result(
+    val adult: Boolean,
+    val backdrop_path: String,
+    val genre_ids: List<Int>,
+    val id: Int,
+    val original_language: String,
+    val original_title: String,
+    val overview: String,
+    val popularity: Double,
+    val poster_path: String,
+    val release_date: String,
+    val title: String,
+    val video: Boolean,
+    val vote_average: Double,
+    val vote_count: Int
+)
