aefc0ac4703d9d23396bb303003462c94f7b5aef
taeiim <taeiim.god@gmail.com>
2020-02-21 14:57:00 +0900

    BaseActivity 추가

---
+++ MovieApp/app/src/main/java/com/god/taeiim/movieapp/base/BaseActivity.kt
@@ -0,0 +1,20 @@
+package com.god.taeiim.movieapp.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(
+    @LayoutRes private val layoutResId: Int
+) : AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutResId)
+    }
+
+}
