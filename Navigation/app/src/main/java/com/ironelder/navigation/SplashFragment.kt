ca2ccb18f7580c4c803d8e35cef97abc390eecb3
JangJinHyuk <jhjang@jangboo.kr>
2020-03-05 19:12:25 +0900

    RN 앱의 껍데기 끼워넣기
    RN 앱의 껍데기 끼워넣기

--- Navigation/app/src/main/java/com/ironelder/navigation/SplashFragment.kt
+++ Navigation/app/src/main/java/com/ironelder/navigation/SplashFragment.kt
@@ -2,29 +2,33 @@ package com.ironelder.navigation
 
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_login.*
 
-class LoginFragment : Fragment() {
+/**
+ * A simple [Fragment] subclass.
+ */
+class SplashFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        activity?.window?.statusBarColor = Color.WHITE
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        activity?.window?.statusBarColor = Color.parseColor("#F7D931")
+        return inflater.inflate(R.layout.fragment_splash, container, false)
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        btn_login.setOnClickListener {
+
+        Handler().postDelayed({
             activity?.let {
-                findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
+                findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
             }
-        }
+        }, 2500)
     }
 }
