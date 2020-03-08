c37aaab58477f04c3cb42f63bd9c4a5242acb57d
handnew <handnew04@gmail.com>
2020-03-09 00:13:56 +0900

    coroutine 사용

--- ToyProject/app/src/main/java/com/example/toyproject/ui/main/MainViewModel.kt
+++ ToyProject/app/src/main/java/com/example/toyproject/ui/main/MainViewModel.kt
@@ -5,17 +5,26 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.toyproject.data.entity.SearchMovieData
 import com.example.toyproject.data.repository.MovieRepositoryImpl
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainViewModel() : ViewModel() {
     val TAG = this.javaClass.name
     val movieData = MutableLiveData<List<SearchMovieData>>()
     val query = MutableLiveData<String>()
+    val isLoading = MutableLiveData<Boolean>()
 
     fun searchMovie() {
-        MovieRepositoryImpl.getMovieData(query.toString(), success = {
-            movieData.value = it.searchMovieResults
-        }, fail = {
-            Log.e(TAG, it.message)
-        })
+        runBlocking {
+            launch {
+                MovieRepositoryImpl.getMovieData(query.toString(), success = {
+                    movieData.value = it.searchMovieResults
+                }, fail = {
+                    Log.e(TAG, it.message)
+                })
+            }
+            isLoading.value = true
+        }
+        isLoading.value = false
     }
 }
