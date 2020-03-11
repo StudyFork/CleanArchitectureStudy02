d1721bf620313a1a5970689e486beea0ccf5f698
zion830 <zion830@honglab.org>
2020-03-11 21:47:20 +0900

    feat: response data class 추가

---
+++ CleanArchitectureStudy/app/src/main/java/yunji/cleanarchitecturestudy02/model/response/MovieListResponse.kt
@@ -0,0 +1,15 @@
+package yunji.cleanarchitecturestudy02.model.response
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieListResponse(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val movies: List<Movie>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
---
+++ CleanArchitectureStudy/app/src/main/java/yunji/cleanarchitecturestudy02/model/response/SpokenLanguage.kt
@@ -0,0 +1,11 @@
+package yunji.cleanarchitecturestudy02.model.response
+
+
+import com.google.gson.annotations.SerializedName
+
+data class SpokenLanguage(
+    @SerializedName("iso_639_1")
+    val iso6391: String,
+    @SerializedName("name")
+    val name: String
+)
