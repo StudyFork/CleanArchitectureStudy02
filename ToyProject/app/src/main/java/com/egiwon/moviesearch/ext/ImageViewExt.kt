970d0bb4fed029684583c4663c6a296969874d0d
2giwon <2giwon@gmail.com>
2020-03-17 19:47:47 +0900

    #03 포스터 이미지 요청시 '/' 코드 제거

--- ToyProject/app/src/main/java/com/egiwon/moviesearch/ext/ImageViewExt.kt
+++ ToyProject/app/src/main/java/com/egiwon/moviesearch/ext/ImageViewExt.kt
@@ -9,9 +9,9 @@ import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 fun ImageView.loadAsyncImage(url: String) {
 
     Glide.with(this)
-        .load("$POSTER_BASE_URL/$url")
+        .load("$POSTER_BASE_URL$url")
         .transition(DrawableTransitionOptions.withCrossFade())
         .into(this)
 }
 
-const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2"
+const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/"
