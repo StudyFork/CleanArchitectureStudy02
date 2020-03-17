36ed8e1cdb3062ff52a02b541cf0b064e9dd04d0
2giwon <2giwon@gmail.com>
2020-03-17 19:47:16 +0900

    #03 리포지토리 영화 페이징 요청 추가

--- ToyProject/app/src/main/java/com/egiwon/moviesearch/data/MovieRepository.kt
+++ ToyProject/app/src/main/java/com/egiwon/moviesearch/data/MovieRepository.kt
@@ -1,8 +1,17 @@
 package com.egiwon.moviesearch.data
 
+import androidx.lifecycle.LiveData
+import androidx.paging.PagedList
 import com.egiwon.moviesearch.data.model.MovieEntity
 import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
 
 interface MovieRepository {
     fun getPopularMovies(page: Int): Single<List<MovieEntity>>
+
+    fun getPagingPopularMovies(
+        compositeDisposable: CompositeDisposable,
+        onSuccess: (response: List<MovieEntity>) -> Unit,
+        onFailure: (Throwable) -> Unit
+    ): LiveData<PagedList<MovieEntity>>
 }
--- ToyProject/app/src/main/java/com/egiwon/moviesearch/data/MovieRepositoryImpl.kt
+++ ToyProject/app/src/main/java/com/egiwon/moviesearch/data/MovieRepositoryImpl.kt
@@ -1,9 +1,16 @@
 package com.egiwon.moviesearch.data
 
+import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
 import com.egiwon.moviesearch.data.model.MovieEntity
+import com.egiwon.moviesearch.data.source.page.MovieDataSourceFactory
 import com.egiwon.moviesearch.data.source.remote.MovieRemoteDataSource
 import com.egiwon.moviesearch.data.source.remote.response.mapToMovieEntities
+import com.egiwon.moviesearch.ext.onFailureRequestMovie
+import com.egiwon.moviesearch.ext.onSuccessRequestMovie
 import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
 
 class MovieRepositoryImpl(
     private val movieRemoteDataSource: MovieRemoteDataSource
@@ -12,4 +19,16 @@ class MovieRepositoryImpl(
     override fun getPopularMovies(page: Int): Single<List<MovieEntity>> =
         movieRemoteDataSource.getPopularMovies(page)
             .map { movieResponses -> movieResponses.mapToMovieEntities() }
+
+    override fun getPagingPopularMovies(
+        compositeDisposable: CompositeDisposable,
+        onSuccess: onSuccessRequestMovie,
+        onFailure: onFailureRequestMovie
+    ): LiveData<PagedList<MovieEntity>> {
+        val movieDataSourceFactory =
+            MovieDataSourceFactory(compositeDisposable, movieRemoteDataSource, onSuccess, onFailure)
+        return LivePagedListBuilder(movieDataSourceFactory, MovieDataSourceFactory.moviePageConfig)
+            .build()
+    }
+
 }
