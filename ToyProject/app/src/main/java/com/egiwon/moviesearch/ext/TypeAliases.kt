392a140b32dd1ad44ca15535a1f37a2f7227d035
middlefgr <2giwon@gmail.com>
2020-03-19 00:28:31 +0900

    #04 사용하지 않는 코드 제거

--- ToyProject/app/src/main/java/com/egiwon/moviesearch/ext/TypeAliases.kt
+++
@@ -1,6 +0,0 @@
-package com.egiwon.moviesearch.ext
-
-import com.egiwon.moviesearch.data.model.MovieEntity
-
-typealias onSuccessRequestMovie = (response: List<MovieEntity>) -> Unit
-typealias onFailureRequestMovie = (Throwable) -> Unit
