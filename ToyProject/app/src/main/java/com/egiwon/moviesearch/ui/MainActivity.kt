168fbf16c78ef0c388916790f26a8a0389e81571
2giwon <2giwon@gmail.com>
2020-03-18 17:54:19 +0900

    #04 다크 모드 액션바 메뉴 추가

--- ToyProject/app/src/main/java/com/egiwon/moviesearch/ui/MainActivity.kt
+++ ToyProject/app/src/main/java/com/egiwon/moviesearch/ui/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.egiwon.moviesearch.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.Observer
 import com.egiwon.moviesearch.BR
 import com.egiwon.moviesearch.R
@@ -28,5 +31,33 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.a
                 replaceAll(it)
             }
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+        val nightMode = AppCompatDelegate.getDefaultNightMode()
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            menu?.findItem(R.id.night_mode)?.setIcon(R.drawable.ic_sunny)
+        } else {
+            menu?.findItem(R.id.night_mode)?.setIcon(R.drawable.ic_brightness)
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.night_mode -> {
+                val nightMode = AppCompatDelegate.getDefaultNightMode()
+                if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+                recreate()
+            }
+
+        }
+        return true
     }
 }
