e78e37d19a7a01eaa20eca0f8fbc9346d8fabbc8
2giwon <2giwon@gmail.com>
2020-03-19 18:50:35 +0900

    #05 사용하지 않는 변수 제거

--- ToyProject/app/src/main/java/com/egiwon/moviesearch/data/model/MovieEntity.kt
+++ ToyProject/app/src/main/java/com/egiwon/moviesearch/data/model/MovieEntity.kt
@@ -1,20 +1,16 @@
 package com.egiwon.moviesearch.data.model
 
 import com.egiwon.moviesearch.base.BaseIdentifier
-import com.egiwon.moviesearch.ui.vo.MovieViewObject
+import com.egiwon.moviesearch.ui.model.MovieViewObject
 
 data class MovieEntity(
     override val id: Int = 0,
     val title: String = "",
-    val population: Double = 0.0,
-    val posterPath: String = "",
-    val overView: String = ""
+    val posterPath: String = ""
 ) : BaseIdentifier()
 
 fun MovieEntity.mapToMovieViewObject(): MovieViewObject = MovieViewObject(
     id = id,
     title = title,
-    population = population,
-    posterPath = posterPath,
-    overView = overView
+    posterPath = posterPath
 )
