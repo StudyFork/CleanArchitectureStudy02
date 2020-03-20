332a6c50bd2b2d3739b25c5a05c118b96474c151
machine-human <dhwltn02@gmail.com>
2020-03-21 03:23:23 +0900

    [ADD] 마지막 페이지시 예외 처리 추가

--- Coroutine/app/src/main/java/com/mhuman/coroutine/data/remote/PopularDataSource.kt
+++ Coroutine/app/src/main/java/com/mhuman/coroutine/data/remote/PopularDataSource.kt
@@ -12,8 +12,9 @@ import org.koin.core.inject
 
 class PopularDataSource(private val scope: CoroutineScope) : PageKeyedDataSource<Int, Result>(),
     KoinComponent {
-
+    private var totalPage = 0
     private val movieApi: MovieApi by inject()
+
     val networkErrors: MutableLiveData<String> = MutableLiveData()
 
     override fun loadInitial(
@@ -25,9 +26,10 @@ class PopularDataSource(private val scope: CoroutineScope) : PageKeyedDataSource
                 movieApi.getPopularMovieList(BuildConfig.THE_MOVIE_API_KEY, page = FIRST_PAGE)
             when {
                 response.isSuccessful ->
-                    response.body()?.results?.let {
+                    response.body()?.let {
+                        totalPage = it.totalPages
                         callback.onResult(
-                            it,
+                            it.results,
                             null,
                             FIRST_PAGE + 1
                         )
@@ -43,6 +45,11 @@ class PopularDataSource(private val scope: CoroutineScope) : PageKeyedDataSource
         params: LoadParams<Int>,
         callback: LoadCallback<Int, Result>
     ) {
+        if (params.key > totalPage) {
+            networkErrors.postValue(LAST_PAGE_NOTI_MESSAGE)
+            return
+        }
+
         scope.launch {
             val response =
                 movieApi.getPopularMovieList(BuildConfig.THE_MOVIE_API_KEY, page = params.key)
@@ -73,6 +80,7 @@ class PopularDataSource(private val scope: CoroutineScope) : PageKeyedDataSource
         const val FIRST_PAGE = 1
         const val ERROR_MESSAGE_REQUEST_FAILED = "데이터 요청을 실패하였습니다.\n네트워크 설정을 확인해주세요."
         const val ERROR_MESSAGE_EMPTY_DATA = "찾으시는 영화 데이터가 \n서버에 존재하지 않습니다."
+        const val LAST_PAGE_NOTI_MESSAGE = "마지막 페이지입니다."
     }
 }
 
