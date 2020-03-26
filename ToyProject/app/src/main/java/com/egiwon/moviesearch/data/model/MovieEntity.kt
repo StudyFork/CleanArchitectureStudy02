3e8442991999299e11a3e5a0536fefb96787cecd
2giwon <2giwon@gmail.com>
2020-03-26 18:33:26 +0900

    #05 사용하지 않는 코드 제거

--- ToyProject/app/src/main/java/com/egiwon/moviesearch/data/model/MovieEntity.kt
+++ ToyProject/app/src/main/java/com/egiwon/moviesearch/data/model/MovieEntity.kt
@@ -1,7 +1,6 @@
 package com.egiwon.moviesearch.data.model
 
 import com.egiwon.moviesearch.base.BaseIdentifier
-import com.egiwon.moviesearch.ui.model.MovieViewObject
 
 data class MovieEntity(
     override val id: Int = 0,
@@ -9,8 +8,3 @@ data class MovieEntity(
     val posterPath: String = ""
 ) : BaseIdentifier()
 
-fun MovieEntity.mapToMovieViewObject(): MovieViewObject = MovieViewObject(
-    id = id,
-    title = title,
-    posterPath = posterPath
-)
--- ToyProject/app/src/main/java/com/egiwon/moviesearch/ui/model/MovieViewObject.kt
+++
@@ -1,9 +0,0 @@
-package com.egiwon.moviesearch.ui.model
-
-import com.egiwon.moviesearch.base.BaseIdentifier
-
-data class MovieViewObject(
-    override val id: Int,
-    val title: String,
-    val posterPath: String
-) : BaseIdentifier()
