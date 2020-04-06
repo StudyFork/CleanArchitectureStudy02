66b0ab9ef01aa50a0c293fa2f6dd3da9c6616803
middlefgr <2giwon@gmail.com>
2020-04-07 00:24:29 +0900

    #06 glide 함수 추가 및 requestOption을 전달 받도록 수정

--- ToyProject/app/src/main/java/com/egiwon/moviesearch/ext/ImageViewExt.kt
+++ ToyProject/app/src/main/java/com/egiwon/moviesearch/ext/ImageViewExt.kt
@@ -7,3 +7,6 @@ import com.egiwon.moviesearch.wrapper.GlideWrapper
 @BindingAdapter("loadAsyncImage")
 fun ImageView.loadAsyncImage(url: String?) = GlideWrapper.asyncLoadImage(this, url)
 
+@BindingAdapter("loadAsyncCastImage")
+fun ImageView.loadAsyncCastImage(url: String?) = GlideWrapper.asyncLoadCircleImage(this, url)
+
--- ToyProject/app/src/main/java/com/egiwon/moviesearch/wrapper/GlideWrapper.kt
+++ ToyProject/app/src/main/java/com/egiwon/moviesearch/wrapper/GlideWrapper.kt
@@ -4,16 +4,37 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 
 object GlideWrapper {
+    private fun asyncLoadImage(
+        target: ImageView,
+        url: String?,
+        block: RequestOptions.() -> RequestOptions
+    ) {
+        val options = RequestOptions()
+        options.block()
 
-    fun asyncLoadImage(target: ImageView, url: String?) {
         Glide.with(target)
             .load("$POSTER_BASE_URL$url")
+            .apply(options)
             .transition(DrawableTransitionOptions.withCrossFade())
             .diskCacheStrategy(DiskCacheStrategy.NONE)
             .skipMemoryCache(true)
             .into(target)
+    }
+
+
+    fun asyncLoadImage(target: ImageView, url: String?) {
+        asyncLoadImage(target, url) {
+            RequestOptions()
+        }
+    }
+
+    fun asyncLoadCircleImage(target: ImageView, url: String?) {
+        asyncLoadImage(target, url) {
+            RequestOptions.centerCropTransform()
+        }
     }
 
     private const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/"
