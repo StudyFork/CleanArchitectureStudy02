d49b7b7dd30ed2bc0710febf2327c4f18819c9ce
middlefgr <2giwon@gmail.com>
2020-04-07 23:45:02 +0900

    #06 BaseRecyclerView에서 ListAdapter용 BaseAdapter 추가

--- ToyProject/app/src/main/java/com/egiwon/moviesearch/base/BaseRecyclerView.kt
+++ ToyProject/app/src/main/java/com/egiwon/moviesearch/base/BaseRecyclerView.kt
@@ -8,6 +8,7 @@ import androidx.databinding.ViewDataBinding
 import androidx.paging.PagedList
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
 abstract class BaseRecyclerView {
@@ -44,12 +45,48 @@ abstract class BaseRecyclerView {
         open fun onRecycledViewHolder() = Unit
     }
 
-    abstract class BaseAdapter<IDENTIFIER : BaseIdentifier, VDB : ViewDataBinding>(
+    abstract class BasePagedAdapter<IDENTIFIER : BaseIdentifier, VDB : ViewDataBinding>(
         @LayoutRes private val layoutResId: Int,
         private val bindingId: Int,
         private val viewModels: Map<Int?, BaseViewModel> = mapOf()
-    ) : PagedListAdapter<IDENTIFIER, BaseViewHolder<VDB>>(object :
+    ) : PagedListAdapter<IDENTIFIER, BaseViewHolder<VDB>>(
+        object : DiffUtil.ItemCallback<IDENTIFIER>() {
+            override fun areItemsTheSame(oldItem: IDENTIFIER, newItem: IDENTIFIER): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: IDENTIFIER, newItem: IDENTIFIER): Boolean {
+                return oldItem == newItem
+            }
+
+        }) {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<VDB> =
+            object : BaseViewHolder<VDB>(parent, layoutResId, bindingId, viewModels) {}
+
+        override fun onBindViewHolder(holder: BaseViewHolder<VDB>, position: Int) =
+            holder.onBindViewHolder(getItem(position))
+
+        override fun onViewRecycled(holder: BaseViewHolder<VDB>) {
+            super.onViewRecycled(holder)
+            holder.onRecycledViewHolder()
+        }
+
+        fun replaceAllPagedItems(items: PagedList<IDENTIFIER>?) {
+            if (items != null) {
+                submitList(items)
+            }
+        }
+
+    }
+
+    abstract class BaseListAdapter<IDENTIFIER : BaseIdentifier, VDB : ViewDataBinding>(
+        @LayoutRes private val layoutResId: Int,
+        private val bindingId: Int,
+        private val viewModels: Map<Int?, BaseViewModel> = mapOf()
+    ) : ListAdapter<IDENTIFIER, BaseViewHolder<VDB>>(object :
         DiffUtil.ItemCallback<IDENTIFIER>() {
+
         override fun areItemsTheSame(oldItem: IDENTIFIER, newItem: IDENTIFIER): Boolean {
             return oldItem.id == newItem.id
         }
@@ -71,7 +108,7 @@ abstract class BaseRecyclerView {
             holder.onRecycledViewHolder()
         }
 
-        fun replaceAll(items: PagedList<IDENTIFIER>?) {
+        fun replaceAll(items: List<IDENTIFIER>?) {
             if (items != null) {
                 submitList(items)
             }
