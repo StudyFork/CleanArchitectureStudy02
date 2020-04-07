55dbfed3e56358aa48e37645b311fbb4224f8134
handnew <handnew04@gmail.com>
2020-04-08 03:42:57 +0900

    runblocking 을 viewmodelScope 로 변경

--- ToyProject/app/src/main/java/com/example/toyproject/ui/main/MainViewModel.kt
+++ ToyProject/app/src/main/java/com/example/toyproject/ui/main/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.example.toyproject.ui.main
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.toyproject.data.entity.SearchMovieData
 import com.example.toyproject.data.repository.MovieRepositoryImpl
 import kotlinx.coroutines.launch
@@ -15,15 +16,13 @@ class MainViewModel() : ViewModel() {
     val isLoading = MutableLiveData<Boolean>()
 
     fun searchMovie() {
-        runBlocking {
-            launch {
+        showLoading()
+        viewModelScope.launch {
                 MovieRepositoryImpl.getMovieData(query.value!!, success = {
                     movieData.value = it.searchMovieResults
                 }, fail = {
                     Log.e(TAG, it.message)
                 })
-            }
-            showLoading()
         }
         hideLoading()
     }
